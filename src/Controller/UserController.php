b5acf364e5911e3b44705982c7c8569bd882fc07
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-07-15 20:27:24 +0300

    Switched to using Form Type for the register user form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use App\Form\Type\UserFormType;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
@@ -25,14 +26,8 @@ class UserController extends AbstractController
 
     public function registerUserPage(Environment $twig, Request $request) : Response {
         $user = new User();
-        $form = $this->createFormBuilder($user);
-        $form->add('first_name', TextType::class);
-        $form->add('last_name', TextType::class);
-        $form->add('username', TextType::class);
-        $form->add('email', TextType::class);
-        $form->add('phone', TextType::class);
-        $form->add('password', TextType::class);
-        $form = $form->getForm();
+
+        $form = $this->createForm(UserFormType::class, $user);
         $form = $form->createView();
 
         return new Response($twig->render('user/register_user.html.twig', ['form' => $form]));
