192fe57f78b1b332801c53f8c657b20494ab2014
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-07-30 17:05:08 +0300

    Worked with setting up a test database, added SoppingCartItem Entity

---
+++ src/Entity/ShoppingCartItem.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class ShoppingCartItem
+{
+
+    private $product_id;
+    private $product_name;
+
+    public function getProductId() : int {
+        return $this->product_id;
+    }
+
+    public function setProductId(int $id) : void {
+        $this->product_id = $id;
+    }
+
+    public function getProductName() : string {
+        return $this->product_name;
+    }
+
+    public function setProductName(string $product_name) : void {
+        $this->product_name = $product_name;
+    }
+
+}
