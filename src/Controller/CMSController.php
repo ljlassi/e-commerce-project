37979d999d58b940d7f7edce6d860dfee7598386
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-08-06 18:03:47 +0300

    Now able to upload banner image for front page, further work needed with e.g. displaying the image at front page

--- src/Controller/CMSController.php
+++ src/Controller/CMSController.php
@@ -4,9 +4,15 @@
 namespace App\Controller;
 
 
+use App\Entity\CMSBanner;
+use App\Form\Type\CMSBannerFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Twig\Environment;
 
 class CMSController extends AbstractController
@@ -22,8 +28,49 @@ class CMSController extends AbstractController
      * @throws \Twig\Error\SyntaxError
      */
 
-    public function changeBannerView(Environment $twig) : Response {
-        return new Response($twig->render('admin/cms/change_banner.html.twig', []));
-    }
+    public function changeBanner(Environment $twig, Request $request, SluggerInterface $slugger, EntityManagerInterface $entityManager): Response
+    {
+        $cmsBanner = new CMSBanner();
+        $form = $this->createForm(CMSBannerFormType::class, $cmsBanner);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imageUrl')->getData();
+
+            // this condition is needed because the 'image' field is not required
+            // so the image file must be processed only when a file is uploaded
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    echo $e->getMessage();
+                }
+
+                // updates the 'imageFilename' property to store the image file name
+                // instead of its contents
+                $cmsBanner->setImageUrl($newFilename);
+            }
+            $cmsBanner = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($cmsBanner);
+            $entityManager->flush();
+
+            return new Response('Succesfully changed banner image', 200);
+
+        }
+        else {
+            $form = $form->createView();
+            return new Response($twig->render('admin/cms/change_banner.html.twig', ['form' => $form]));
+        }
+    }
 }
---
+++ src/Form/Type/CMSBannerFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+
+class CMSBannerFormType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('imageUrl', FileType::class, [
+            'label' => 'Banner image',
+            'mapped' => false,
+            'required' => true,
+            'constraints' => [
+                new File([
+                    'maxSize' => '2024k',
+                    'mimeTypes' => [
+                        'image/jpeg',
+                        'image/png',
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid PNG or JPEG image.',
+                ])
+            ]
+        ]);
+        $builder->add('alt', TextType::class);
+        $builder->add('submit', SubmitType::class);
+    }
+
+}
