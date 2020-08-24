835721d496cc46716fa19fe274d2098e06eea892
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-08-24 14:51:47 +0300

    Vue now loads

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
+import Vue from 'vue'
+import axios from "axios";
 
 new Vue({
     el: '#app', // where <div id="app"> in your DOM contains the Vue template
-    delimiters: ['${', '}']
+    delimiters: ['${', '}$']
 });
