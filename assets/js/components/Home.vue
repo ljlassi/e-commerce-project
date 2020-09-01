189128f8499b4abaeab4eb36aae9a1e71dccb456
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-09-01 13:24:20 +0300

    Started moving into Vue in home template

---
+++ assets/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>Processing something maybe</h2>
+</template>
+<script>
+    export default {
+        name: "home",
+        methods: {
+            greet: function (event) {
+                // `this` inside methods point to the Vue instance
+                alert('Hello ' + this.name + '!')
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
-//import axios from 'axios'
-//import VueAxios from 'vue-axios'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Example from './components/Example'
+import Home from './components/Home'
 
 new Vue({
     el: '#app', // where <div id="app"> in your DOM contains the Vue template
-    components: {Example},
+    components: {
+        'example' : Example,
+        'home' : Home
+    },
     delimiters: ['${', '}$']
 });
+Vue.use(VueAxios, axios)
