f30e7d4ef9e92002faa111325c5db9db351eea33
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-09-21 13:01:46 +0300

    Added test for ListAllProducts view

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -4,7 +4,16 @@
 namespace App\Tests\Controller;
 
 
-class ProductControllerTest
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProductControllerTest extends WebTestCase
 {
+
+    public function testListAllProducts() {
+        $client = static::createClient();
+        $client->request('GET', '/products/list');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 
 }
